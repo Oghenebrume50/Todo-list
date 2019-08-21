e1f8dec1d3c87b5e4ec697be22b4632e8f0096f3
pwilson77 <p.o.yirenkyi@gmail.com>
2019-08-21 17:42:28 +0000

    worked on handling toggle for displaying todolist description

--- src/displayHandle.js
+++ src/displayHandle.js
@@ -1,11 +1,13 @@
-import handleProject from './project';
+import handleProject from "./project";
+import addExpandToButton from "./eventHandle";
 
-let currentProject = 'default project';
-let id = 'project-1';
+let currentProject = "default project";
+let id = "project-1";
 function displayProjects() {
-  const projectList = document.getElementById('projects-list');
+  const projectList = document.getElementById("projects-list");
   for (let i = 0; i < window.localStorage.length; i += 1) {
-    projectList.innerHTML += ` <button id="project-${i + 1}" class="project-button">${window.localStorage.key(i)}</button>`;
+    projectList.innerHTML += ` <button id="project-${i +
+      1}" class="project-button">${window.localStorage.key(i)}</button>`;
   }
 }
 
@@ -13,45 +15,70 @@ function viewAProject() {
   const projectLi = document.getElementById(this.id);
   currentProject = projectLi.innerText;
   id = this.id;
-  const todoList = document.getElementById('project-todos');
+  const todoList = document.getElementById("project-todos");
   const todoObj = handleProject.getProject(projectLi.innerText);
-  todoList.innerHTML = '';
+  todoList.innerHTML = "";
   for (let i = 0; i < todoObj.todos.length; i += 1) {
-    todoList.innerHTML += `<div class="todo-item">
-    <button class="todo-item-button">${todoObj.todos[i].title}</button>
+    todoList.innerHTML += `<div class="todo-item" id="todo-item-${i}">
+    <button class="todo-item-button" id="todo-btn-${i}">${
+      todoObj.todos[i].title
+    }</button>
     <span>Due: ${todoObj.todos[i].dueDate}</span>
     <button href="#" class="btn-warning">Edit</button> |
     <button href="#" class="btn-danger">Delete</button>
-    <div class="description-container">
+    <div class="description-container hide">
       <p>
         ${todoObj.todos[i].description}
       </p>
     </div>
+    <hr />
   </div>
-  <hr />`;
+ `;
   }
+  addExpandToButton();
 }
 
 function initialProjectView() {
-  const todoList = document.getElementById('project-todos');
+  const todoList = document.getElementById("project-todos");
   const todoObj = handleProject.getProject(currentProject);
-  todoList.innerHTML = '';
+  todoList.innerHTML = "";
   for (let i = 0; i < todoObj.todos.length; i += 1) {
-    todoList.innerHTML += `<div class="todo-item">
-    <button class="todo-item-button">${todoObj.todos[i].title}</button>
+    todoList.innerHTML += `<div class="todo-item" id="todo-item-${i}">
+    <button class="todo-item-button" id="todo-btn-${i}">
+    ${todoObj.todos[i].title}</button>
     <span>Due: ${todoObj.todos[i].dueDate}</span>
     <button href="#" class="btn-warning">Edit</button> |
     <button href="#" class="btn-danger">Delete</button>
-    <div class="description-container">
+    <div class="description-container hide">
       <p>
         ${todoObj.todos[i].description}
       </p>
     </div>
+    <hr />
   </div>
-  <hr />`;
+  `;
+  }
+  console.log(todoObj.todos.length);
+  if (todoObj.todos.length >= 1) {
+    addExpandToButton();
   }
 }
 
-function getCurrentProject() { return currentProject; }
-function getId() { return id; }
-export { displayProjects, viewAProject, getCurrentProject, getId, initialProjectView };
+function getCurrentProject() {
+  return currentProject;
+}
+function getId() {
+  return id;
+}
+
+function expand() {
+  console.log("this is a tesr");
+}
+export {
+  displayProjects,
+  viewAProject,
+  getCurrentProject,
+  getId,
+  initialProjectView,
+  expand
+};
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import {
   submitProject,
   addTodotoProject,
   deleteTodoFromProject
-} from './eventHandle';
-import { displayProjects, initialProjectView } from './displayHandle';
+} from "./eventHandle";
+import { displayProjects, initialProjectView } from "./displayHandle";
 
 const handleTodos = () => {
-  window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener("DOMContentLoaded", () => {
     defaultProject();
-    console.log('test2');
     displayProjects();
     initialProjectView();
+
     submitProject();
     addTodotoProject();
     deleteTodoFromProject();
